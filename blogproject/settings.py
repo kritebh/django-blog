186b216be054643a506f6ef9d0f752a2565de19a
Kritebh <kritebhlaganbibhakar@gmail.com>
2021-06-29 13:06:41 +0530

    Profile model updated

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ip=6c0vxg2xjp'
+SECRET_KEY = 'ip=6c0vxg2xjpuiowefbh^@$#&*^!@(&nwuiefhiweefn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
