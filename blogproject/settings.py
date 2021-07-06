746aa51c8265a1e28c9c00d1dd18888fcb4407c8
Kritebh <kritebhlaganbibhakar@gmail.com>
2021-07-07 00:10:50 +0530

    Updated

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,7 +115,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATICFILES_DIRS = ((os.path.join(BASE_DIR,'static')),)
