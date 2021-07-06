fe44b36a41229a05b12024e220d73a4babec6ca6
Kritebh <kritebhlaganbibhakar@gmail.com>
2021-07-07 01:05:28 +0530

    Updated

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -15,7 +15,6 @@ SECRET_KEY = 'b-a$d436v5!ge67vhrv-b34d_=*%j=pf1*=(41$+gw#((msl$f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = ["majestyblog.herokuapp.com","localhost",'127.0.0.1']
 
 
